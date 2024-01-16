8aff57af669c072b0b40d2afed3627a9bba11501
“Cade <caderosche@gmail.com>
2024-01-15 21:58:50 -0700

    added constants

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,5 @@
 /*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
+Copyright © 2024 Cade Rosche caderosche@gmail.com
 */
 package cmd
 
@@ -9,8 +8,6 @@ import (
 
 	"github.com/spf13/cobra"
 )
-
-
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -24,7 +21,14 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+		gravConstant := 6.67430e-11
+		earthMass := 5.972e24
+		pi := 3.14159
+		println("Gravitational constant", gravConstant, "N*m^2*kg^-2")
+		println("Mass of the earth", earthMass, "kg")
+		println("Pi", pi)
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -47,5 +51,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
