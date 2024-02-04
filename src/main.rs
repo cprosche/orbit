aac7cd783916ef838b509ff8fea45d74632c616a
Cade Rosche <caderosche@gmail.com>
2024-02-04 08:30:39 -0700

    added velocities

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::{f64, rc::Rc};
+
 use clap::{Parser, Subcommand};
 
 // Constants
@@ -6,7 +8,15 @@ const GRAVITATIONAL_CONSTANT: f64 = 6.67430e-11; // N*m^2*kg^-2
 const SEPARATOR: &str = &"------------------------------------------------------";
 
 fn calculate_orbital_period(mass: f64, semi_major_axis: f64) -> f64 {
+    // mass: kg
+    // semi_major_axis: m
     2.0 * PI * ((semi_major_axis.powi(3) / (GRAVITATIONAL_CONSTANT * mass)).sqrt())
+}
+
+fn calculate_circular_orbital_velocity(mass: f64, semi_major_axis: f64) -> f64 {
+    // mass: kg
+    // semi_major_axis: m
+    ((GRAVITATIONAL_CONSTANT * mass) / semi_major_axis).sqrt() // m / s
 }
 
 #[derive(Parser)]
@@ -38,7 +48,7 @@ enum Altitude {
 struct Orbit {
     name: String,
     altitude: Altitude,
-    body: Body,
+    body: Rc<Body>,
 }
 
 impl Orbit {
@@ -78,6 +88,32 @@ impl Orbit {
             }
         }
     }
+
+    fn get_velocity_string(&self) -> String {
+        match &self.altitude {
+            Altitude::Single { value } => {
+                let axis = self.body.radius + value * 1000.0;
+
+                let velocity =
+                    (calculate_circular_orbital_velocity(self.body.mass, axis) * 60.0 * 60.0)
+                        / 1000.0;
+                format!("{velocity:.2} km/hr")
+            }
+            Altitude::Range { max, min } => {
+                let min_axis = self.body.radius + min * 1000.0;
+                let max_axis = self.body.radius + max * 1000.0;
+
+                let min_velocity =
+                    calculate_circular_orbital_velocity(self.body.mass, min_axis) * 60.0 * 60.0
+                        / 1000.0;
+                let max_velocity =
+                    calculate_circular_orbital_velocity(self.body.mass, max_axis) * 60.0 * 60.0
+                        / 1000.0;
+
+                format!("{min_velocity:.2}-{max_velocity:.2} km/s")
+            }
+        }
+    }
 }
 
 fn main() {
@@ -89,10 +125,10 @@ fn main() {
 }
 
 fn earth(altitude: Option<f64>) {
-    let earth = Body {
+    let earth = Rc::new(Body {
         mass: 5.9722e24,
         radius: 6.3781e6,
-    };
+    });
 
     let mut orbits = vec![];
 
@@ -157,6 +193,26 @@ fn earth(altitude: Option<f64>) {
         }
         Altitude::Range { max, min } => {
             println!("{} ({min}-{max} km) \n{}\n", o.name, o.get_period_string());
+        }
+    });
+
+    println!("{SEPARATOR}");
+    println!();
+    println!();
+    println!("Orbital Velocities");
+    println!("{SEPARATOR}");
+    println!();
+
+    orbits.iter().for_each(|o| match &o.altitude {
+        Altitude::Single { value } => {
+            println!("{} ({value} km) \n{}\n", o.name, o.get_velocity_string());
+        }
+        Altitude::Range { max, min } => {
+            println!(
+                "{} ({min}-{max} km) \n{}\n",
+                o.name,
+                o.get_velocity_string()
+            );
         }
     });
 
